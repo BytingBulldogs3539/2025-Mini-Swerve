7dd774a26fd8efafc99322ad8c56591ec451fc41
Programmers3539 <bbprogrammers3539@gmail.com>
2025-01-29 20:58:17 -0500

    Created Swerve Code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+public class Constants {
+    //ID Constants
+    public static int pigeonID = 25;    
+    public static int FLDriveID = 1;
+    public static int FLSteerID = 2;
+    public static int BRDriveID = 3;
+    public static int BRSteerID = 4;
+    public static int BLSteerID = 5;
+    public static int BLDriveID = 6;
+    public static int FRSteerID = 7;
+    public static int FRDriveID = 8;
+    public static int BLCanCoderID = 30;
+    public static int BRCanCoderID = 31;
+    public static int FLCanCoderID = 32;
+    public static int FRCanCoderID = 33;
+    
+    //Drive Constants
+	public static double TranslationkP = 15;
+	public static double TranslationkI = 0;
+	public static double TranslationkD = 0;
+	public static double RotationkP = 5;
+	public static double RotationkI = 0;
+	public static double RotationkD = 0;
+	public static double TranslationkV = 1;
+	public static double TranslationkA = 0;
+	public static double TranslationkS = 0;
+	public static double FLSteerOffset = 0;
+	public static double FRSteerOffset = 0;
+	public static double BLSteerOffset = 0;
+	public static double BRSteerOffset = 0;
+	public static double speedMultiplier = 1;
+	public static double rotationSpeedMultiplier = 0.5;
+	public static double turboSpeedMultiplier = 0.4;
+	public static double turboRotationSpeedMultiplier = 0.3;
+	public static double AlignkP = 2;
+
+
+}
---
+++ src/main/java/frc/robot/DriveSubsystem.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.swerve.*;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import java.util.Arrays;
+import org.frcteam3539.Byte_Swerve_Lib.control.HolonomicMotionProfiledTrajectoryFollower;
+import org.frcteam3539.Byte_Swerve_Lib.control.PidConstants;
+import org.frcteam3539.Byte_Swerve_Lib.control.Trajectory;
+import org.frcteam3539.Byte_Swerve_Lib.util.DrivetrainFeedforwardConstants;
+import org.frcteam3539.Byte_Swerve_Lib.util.HolonomicFeedforward;
+
+public class DriveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> implements Subsystem {
+	/** Creates a new DrivetrainSubsystem. */
+	private SwerveRequest swerveRequest = new SwerveRequest.Idle();
+
+	 public double maxVelocity = 0.0;
+	 public double maxRotationalVelocity = 0.0;
+
+	//public Pigeon pigeon = new Pigeon(Constants.pigeonID, "rio");
+
+	public DriveSubsystem(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+		super(TalonFX::new, TalonFX::new, CANcoder::new, driveTrainConstants, modules);
+
+		maxVelocity = modules[0].SpeedAt12Volts;
+
+		Translation2d[] moduleLocations = new Translation2d[modules.length];
+
+		for (int i = 0; i < modules.length; i++) {
+			moduleLocations[i] = new Translation2d(modules[i].LocationX, modules[i].LocationY);
+			this.getModule(i).getDriveMotor().getConfigurator()
+				.apply(new CurrentLimitsConfigs().withSupplyCurrentLimit(40)
+                        .withSupplyCurrentLimitEnable(true)
+							.withStatorCurrentLimit(modules[i].SlipCurrent).withStatorCurrentLimitEnable(true));
+		}
+
+		double dtRadius = new Translation2d().nearest(Arrays.asList(moduleLocations)).getDistance(new Translation2d());
+		maxRotationalVelocity = (maxVelocity / dtRadius);
+	}
+
+
+	// public void seedFieldRelative(Trajectory trajectory) {
+	// 	this.seedFieldRelative(trajectory.calculate(0).getPathState().getPose2d());
+	// }
+
+	public Pose2d getPose2d() {
+		return this.getState().Pose;
+	}
+
+	public void applyRequest(SwerveRequest request) {
+		this.swerveRequest = request;
+	}
+
+	public void log() {
+	}
+
+	@Override
+	public void periodic() {
+		SwerveRequest request = new SwerveRequest.Idle();
+			request = swerveRequest;
+
+		this.setControl(request);
+		// This method will be called once per scheduler run
+		SmartDashboard.putNumber("/DriveTrain/BatteryVoltage", RobotController.getBatteryVoltage());
+
+	}
+}
